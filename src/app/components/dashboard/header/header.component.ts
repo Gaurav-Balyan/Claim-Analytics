089b23c37d8d0f433181f7e064274be33e546a18
Gaurav Balyan <gaurav.balyan24@gmail.com>
2019-07-27 18:59:19 +0530

    Adding Angular Material

---
+++ src/app/components/dashboard/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  @Output() toggleSideNav = new EventEmitter();
+  toggleFlag = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onHeaderClick() {
+    this.toggleFlag = !this.toggleFlag;
+    this.toggleSideNav.emit(this.toggleFlag);
+  }
+
+}
