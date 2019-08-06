e6730e9098b6879e8a7ddaaa1bd5ddcd6525ec6c
Gaurav Balyan <balyang@HCL.COM>
2019-08-06 22:11:24 +0530

    Adding Header

--- src/app/components/dashboard/header/header.component.ts
+++ src/app/components/dashboard/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +10,20 @@ export class HeaderComponent implements OnInit {
   @Output() toggleSideNav = new EventEmitter();
   @Input() showSidenav: boolean;
   @Input() showCard: boolean;
+  showChip: boolean;
+  userName: string;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
   onHeaderClick() {
     this.showSidenav = !this.showSidenav;
     this.toggleSideNav.emit(this.showSidenav);
+  }
+
+  onLogout() {
+    localStorage.removeItem('ACCESS_TOKEN');
+    this.router.navigate(['./login']);
   }
 }
